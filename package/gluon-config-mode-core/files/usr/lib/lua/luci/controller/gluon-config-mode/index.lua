25898221d8eb70d419380377522a137e390caac7
Kai 'wusel' Siering <wusel+src@uu.org>
2015-05-23 02:54:14 +0200

    Moved presets

--- package/gluon-config-mode-core/files/usr/lib/lua/luci/controller/gluon-config-mode/index.lua
+++ package/gluon-config-mode-core/files/usr/lib/lua/luci/controller/gluon-config-mode/index.lua
@@ -36,9 +36,61 @@ function index()
     page.setgroup = "root"
     page.index    = true
 
+    entry({"gluon-config-mode", "wizard-pre"}, call("prepare"))
     entry({"gluon-config-mode", "wizard-pre"}, form("gluon-config-mode/wizard-pre")).index = true
     entry({"gluon-config-mode", "reboot"}, call("action_reboot"))
   end
+end
+
+function prepare()
+  if uci:get_first("gluon-setup-mode", "setup_mode", "configured") == "0" then
+    -- FIXME! Does this belong here?
+    -- This code sets some presets for our Firmwares.
+    local uci = luci.model.uci.cursor()
+    local secret = uci:get("fastd", "mesh_vpn", "secret")
+
+    if not secret or not secret:match("%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x") then
+      local f = io.popen("fastd --generate-key --machine-readable", "r")
+      local secret = f:read("*a")
+      f:close()
+
+      uci:set("fastd", "mesh_vpn", "secret", secret)
+      uci:save("fastd")
+      uci:commit("fastd")
+
+      uci:set("autoupdater", "settings", "enabled", "1")
+      uci:save("autoupdater")
+      uci:commit("autoupdater")
+
+      uci:set("fastd", "mesh_vpn", "enabled", "1")
+      uci:save("fastd")
+      uci:commit("fastd")
+
+      uci:set("gluon-simple-tc", "mesh_vpn", "interface")
+      uci:set("gluon-simple-tc", "mesh_vpn", "ifname", "mesh-vpn")
+      uci:set("gluon-simple-tc", "mesh_vpn", "enabled", "0")
+      uci:save("gluon-simple-tc")
+      uci:commit("gluon-simple-tc")
+
+      local sname = uci:get_first("gluon-node-info", "location")
+      uci:set("gluon-node-info", sname, "share_location", "1")
+      uci:save("gluon-node-info")
+      uci:commit("gluon-node-info")
+    end
+
+    -- If there's no location set, try to get something via callback, as we need this for
+    -- selecting the proper settings.
+    -- local lat = uci:get_first("gluon-node-info", 'location', "latitude")
+    -- local lon = uci:get_first("gluon-node-info", 'location', "longitude")
+    -- if not lat or not lon then
+    --  os.execute('sh "/lib/gluon/ffgt-geolocate/senddata.sh"')
+    --  os.execute('sleep 20')
+    -- end
+    -- if not uci:get("ffwizard","settings","runbefore") then
+    --   luci.http.redirect(luci.dispatcher.build_url("admin/freifunk/assistent/changePassword"))
+    -- else
+    --   luci.http.redirect(luci.dispatcher.build_url("admin/freifunk/assistent/generalInfo"))
+    -- end
 end
 
 function action_reboot()
