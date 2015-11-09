916eaec841cbfd1953c7c9b6202f2a989fc60d06
Kai 'wusel' Siering <wusel+src@uu.org>
2015-11-09 20:06:07 +0100

    Refactored for CC

--- package/gluon-announce/files/usr/lib/lua/gluon/announce.lua
+++ package/gluon-announce/files/usr/lib/lua/gluon/announce.lua
@@ -2,13 +2,13 @@
 
 module('gluon.announce', package.seeall)
 
-fs = require 'luci.fs'
+fs = require 'nixio.fs'
 json = require 'luci.json'
 uci = require('luci.model.uci').cursor()
 util = require 'luci.util'
 
 local function collect_entry(entry)
-	if fs.isdirectory(entry) then
+	if fs.stat(entry, 'type') == 'dir' then
 		return collect_dir(entry)
 	else
 		return setfenv(loadfile(entry), _M)()
@@ -18,7 +18,7 @@ end
 function collect_dir(dir)
 	local ret = { [json.null] = true }
 
-	for _, entry in ipairs(fs.dir(dir)) do
+	for entry in fs.dir(dir) do
 		if entry:sub(1, 1) ~= '.' then
 			local ok, val = pcall(collect_entry, dir .. '/' .. entry)
 			if ok then
