991166ff9a5807de3be6700b5e382ce2e029f64b
wusel42 <wusel+src@uu.org>
2016-05-31 19:49:19 +0200

    Fix some leftovers from the merge.

--- package/gluon-mesh-batman-adv-core/check_site.lua
+++ package/gluon-mesh-batman-adv-core/check_site.lua
@@ -9,21 +9,7 @@ for _, config in ipairs({'wifi24', 'wifi5'}) do
    need_number(config .. '.mesh_mcast_rate')
    need_number(config .. '.mesh_vlan', false)
    need_boolean(config .. '.client_disabled', false)
---   need_boolean(config .. '.adclient_disabled', false)
    need_boolean(config .. '.mesh_disabled', false)
---   if need_string(config .. '.mesh_enc', false) then
---      need_string(config .. '.mesh_psk')
---   end
-
---   need_boolean(config .. '.stamesh_enabled', false)
---   if need_string(config .. '.stamesh_ssid', false) then
---      if not need_string(config .. '.stamesh_enc', false) then
---         need_string(config .. '.mesh_enc')
---      end
---      if not need_string(config .. '.stamesh_psk', false) then
---         need_string(config .. '.mesh_psk')
---      end
---   end
 end
 
 need_number('site_vlan', false)
