6e76ae0a90c723bc52aa88b574a35b66e1e24d4c
wusel42 <wusel+src@uu.org>
2016-06-11 10:41:41 +0200

    Use fake site.conf checker ...

--- scripts/check_site.sh
+++ scripts/check_site.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# 2016-06-11: Using a fake version as the host's lua can't cope with require('luci') etc.
 SITE_CONFIG_LUA=package/gluon-core/files/usr/lib/lua/gluon/site_config_fake.lua
 CHECK_SITE_LIB=scripts/check_site_lib.lua
 
