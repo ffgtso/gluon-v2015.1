1f90467e936d54b7b72c248e4bbb84966058062a
wusel42 <wusel+src@uu.org>
2016-06-11 15:21:58 +0200

    Use fake site.conf checker ...

--- scripts/site.sh
+++ scripts/site.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-SITE_CONFIG_LUA=package/gluon-core/files/usr/lib/lua/gluon/site_config.lua
+# 2016-06-11: Using a fake version as the host's lua can't cope with require('luci') etc.
+SITE_CONFIG_LUA=package/gluon-core/files/usr/lib/lua/gluon/site_config_fake.lua
 
 "$GLUONDIR"/openwrt/staging_dir/host/bin/lua -e "print(assert(dofile(os.getenv('GLUONDIR') .. '/${SITE_CONFIG_LUA}').$1))" 2>/dev/null
