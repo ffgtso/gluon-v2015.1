036fd871e79e7050fb7ac9c05082f2d451a1faf8
wusel42 <wusel+src@uu.org>
2017-05-05 02:08:34 +0200

    Bit confused; we don't need this at two locations ...

--- package/gluon-setup-mode/files/lib/gluon/upgrade/001-aaaerly-save-wan-mac.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-( WANMAC="`uci get network.wan.macaddr`" ; \
- if [ ! -z "${WANMAC}" ]; then \
-  uci get system.@system[0].staticwanmac || uci set system.@system[0].staticwanmac="${WANMAC}" ;\
-  uci commit ;\
- fi ) >/dev/null 2>&1
